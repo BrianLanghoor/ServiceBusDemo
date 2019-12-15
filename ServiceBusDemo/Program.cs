f212669727c7640756a032451aaca044245c1b22
Brian Langhoor <brian.langhoor@infocaster.net>
2019-12-15 15:29:27 +0100

    Add project files.

---
+++ ServiceBusDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace ServiceBusPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendMessageToQueue().GetAwaiter().GetResult();
+        }
+
+        private static async Task SendMessageToQueue()
+        {
+            var queueClient = new QueueClient("<<<SAS ServiceBus connection string>>>", "<<<Queue name>>>");
+            Console.WriteLine("This is the publisher");
+            while (true)
+            {
+                Console.WriteLine("Write the message to send");
+                var message = Console.ReadLine();
+
+                if (message == "exit")
+                {
+                    Console.WriteLine("Stopping publisher");
+                    break;
+                }
+
+                var body = new Message(Encoding.UTF8.GetBytes(message));
+                Console.WriteLine($"Sending message");
+                await queueClient.SendAsync(body);
+            }
+        }
+    }
+}
---
+++ ServiceBusListener/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace ServiceBusListener
+{
+    class Program
+    {
+        static IQueueClient _queueClient;
+        static void Main(string[] args)
+        {
+            _queueClient =new QueueClient("<<<SAS queue connection string>>>", "<<<Queue name>>>");
+            
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+            _queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+
+            Console.ReadLine();
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            // Process the message
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+            // Complete the message so that it is not received again.
+            // This can be done only if the queueClient is created in ReceiveMode.PeekLock mode (which is default).
+            await _queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+    }
+}
